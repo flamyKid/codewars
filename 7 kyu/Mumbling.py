2cf8b83e8d5d17e094f5cc319cd72d538edf78b2
Seraphim Ryaboshapko <70561906+ityas@users.noreply.github.com>
2021-09-16 13:12:28 +0700

    Create Mumbling.py

---
+++ 7 kyu/Mumbling.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+def accum(s):
+    answer = ''
+    count = 0
+    
+    for char in s:
+        answer += char.upper() + (char.lower() * count) + '-'
+        count += 1
+
+    return answer[0:-1]  # # строка без тире в конце
