16988e679ee5a187ac3e3a69c871dc4b469cc881
Seraphim Ryaboshapko <70561906+ityas@users.noreply.github.com>
2021-09-16 13:06:29 +0700

    Create Interview Question (easy).py

---
+++ 7 kyu/Interview Question (easy).py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/5b358a1e228d316283001892
+def get_strings(city):
+    count_char = {}
+    
+    for char in city.lower():
+        if char in count_char:
+            count_char[char] += '*'
+        else:
+            if char != ' ':  # чтобы не записывало пробелы
+                count_char[char] = '*'
+            
+    # преобразование в одну строку
+    answer = ''
+    for char in count_char:
+        answer += f'{char}:{count_char[char]},'
+    
+    return answer[0:-1]
