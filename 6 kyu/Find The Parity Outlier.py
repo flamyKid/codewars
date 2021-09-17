1b38e7ed34b9db5e57521ca7e7db1ab55d1e0317
ityas <ryaboshapkoseraph@gmail.com>
2021-09-17 16:09:59 +0700

    update task

--- 6 kyu/Find The Parity Outlier.py
+++ 6 kyu/Find The Parity Outlier.py
@@ -14,14 +14,3 @@ def find_outlier(integers):
         return odd[0]
     else:
         return even[0]
-
-
-def test(data, answer):
-    assert data == answer, f'{data} should be {answer}'
-
-
-if __name__ == '__main__':
-    test(find_outlier([2, 4, 6, 8, 10, 3]), 3)
-    test(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36]), 11)
-    test(find_outlier([160, 3, 1719, 19, 11, 13, -21]), 160)
-    print('___ Everything test passed ___')
