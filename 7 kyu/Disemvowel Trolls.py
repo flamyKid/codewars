c0d71163c9d81076b9a2dbc0f5f22c9431ea535b
Seraphim Ryaboshapko <70561906+flamyKid@users.noreply.github.com>
2023-06-21 19:09:20 +0700

    Update Disemvowel Trolls.py

--- 7 kyu/Disemvowel Trolls.py
+++ 7 kyu/Disemvowel Trolls.py
@@ -1,11 +1,10 @@
 # https://www.codewars.com/kata/52fba66badcd10859f00097e
 
 def disemvowel(string):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    final_string = ''
+    answer = ''
     
     for char in string:
-        if char.lower() not in vowels:
-            final_string += char
-    
-    return final_string
+        if char.lower() not in "aeiou":
+            answer += char
+            
+    return answer
