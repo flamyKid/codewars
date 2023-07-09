31f0ecdf5f89e501bb51674565af137458d6ecf2
Seraphim Ryaboshapko <70561906+flamyKid@users.noreply.github.com>
2023-07-09 22:53:27 +0700

    Update Create Phone Number.py

--- 6 kyu/Create Phone Number.py
+++ 6 kyu/Create Phone Number.py
@@ -1,4 +1,4 @@
 # https://www.codewars.com/kata/525f50e3b73515a6db000b83
 
-def create_phone_number(n):
-    return f'({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}'
+def create_phone_number(numbers_list):
+	return '({}{}{}) {}{}{}-{}{}{}{}'.format(*numbers_list)
